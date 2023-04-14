fb84c13574690b73a10264e7d828068bc89fcb4d
FredericoGSilva <frederico.dev@outlook.com>
2023-04-14 20:51:31 -0300

    método parse

--- API_Filmes/filmes/src/JsonParser.java
+++ API_Filmes/filmes/src/JsonParser.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class JsonParser {
@@ -5,6 +10,37 @@ public class JsonParser {
     private static final Pattern REGEX_ITEMS = Pattern.compile(".*\\[(.+)\\].*");
     private static final Pattern REGEX_ATRIBUTOS_JSON = Pattern.compile("\"(.+?)\":\"(.*?)\"");
  
-    
+    public List<Map<String, String>> parse(String json) {
+
+        Matcher matcher = REGEX_ITEMS.matcher(json);
+        
+        if (!matcher.find()) {
+            throw new IllegalArgumentException("Não encontrou items.");
+        }
+
+        String[] itensFilmes = matcher.group(1).split("\\},\\{");
+
+        List<Map<String, String>> dados = new ArrayList<>();
+
+        for (String item : itensFilmes) {
+
+            Map<String, String> atributosItem = new HashMap<>();
+            
+            Matcher matcherAtributosJson = REGEX_ATRIBUTOS_JSON.matcher(item);
+
+            while (matcherAtributosJson.find()) {
+                String nomeAtributo = matcherAtributosJson.group(1);
+                String valorAtributo = matcherAtributosJson.group(2);
+                atributosItem.put(nomeAtributo, valorAtributo);
+            }
+
+            dados.add(atributosItem);
+            
+            
+        }
+        
+        return dados;
+
+    }
     
 }
